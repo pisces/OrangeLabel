c8ffce0a9d78c8f275952a36fdaf4ef65460637d
Steve Kim <pisces@retrica.co>
2017-09-26 14:56:31 +0900

    Support to utf16 characters with regular expression

--- OrangeLabel/Classes/String+OrangeLabel.swift
+++ OrangeLabel/Classes/String+OrangeLabel.swift
@@ -10,9 +10,8 @@ import Foundation
 
 extension String {
     func matches(pattern: String) throws -> [NSTextCheckingResult] {
-        let regex = try NSRegularExpression(pattern: pattern, options: .caseInsensitive)
-        let range = NSRange(location: 0, length: self.characters.count)
-        return regex.matches(in: self, options: .reportCompletion, range: range)
+        let regex = try NSRegularExpression(pattern: pattern)
+        return regex.matches(in: self, options: .reportCompletion, range: NSRange(0..<utf16.count))
     }
     func nsRange(from range: Range<String.Index>) -> NSRange {
         let from = range.lowerBound.samePosition(in: utf16)
@@ -21,8 +20,7 @@ extension String {
                        length: utf16.distance(from: from, to: to))
     }
     func range(from nsRange: NSRange) -> Range<String.Index>? {
-        guard
-            let from16 = utf16.index(utf16.startIndex, offsetBy: nsRange.location, limitedBy: utf16.endIndex),
+        guard let from16 = utf16.index(utf16.startIndex, offsetBy: nsRange.location, limitedBy: utf16.endIndex),
             let to16 = utf16.index(from16, offsetBy: nsRange.length, limitedBy: utf16.endIndex),
             let from = String.Index(from16, within: self),
             let to = String.Index(to16, within: self)
